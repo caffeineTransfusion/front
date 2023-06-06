bff70f1e833d6eb1a29ebb9c9d97ed1603863cdc
dong-fa <120082854+dong-fa@users.noreply.github.com>
2023-06-06 21:52:30 +0900

    design /  COF-27 로그인 CSS
    헤더에서 로그인, 회원가입 눌렀을때 해당하는 페이지 모달로 오픈됨
    
    로그인 페이지 CSS 진행중

--- src/component/main/Header.tsx
+++ src/component/main/Header.tsx
@@ -1,24 +1,39 @@
 import React, { useState, useCallback } from 'react'
 import '../../styles/main/Header.scss'
 import Modal from './Modal'
+import Login from './Login'
+import SignUp from './SignUp'
 
 const Header: React.FC = () => {
   const [isModalOpen, setModalOpen] = useState<boolean>(false)
+  const [showLogin, setShowLogin] = useState<boolean>(true)
 
   const onClickToggleModal = useCallback(() => {
     setModalOpen(!isModalOpen)
   }, [isModalOpen])
+
+  const onClickLogin = useCallback(() => {
+    setShowLogin(true)
+    setModalOpen(true)
+  }, [])
+
+  const onClickSignUp = useCallback(() => {
+    setShowLogin(false)
+    setModalOpen(true)
+  }, [])
 
   return (
     <div className="headerContainer">
       <div className="headerWrap">
         <h2>당커땡</h2>
         <div className="headerButtonWrap">
-          <button onClick={onClickToggleModal}>로그인</button>
-          <button onClick={onClickToggleModal}>회원가입</button>
+          <button onClick={onClickLogin}>로그인</button>
+          <button onClick={onClickSignUp}>회원가입</button>
         </div>
       </div>
-      {isModalOpen && <Modal onClickToggleModal={onClickToggleModal}>나와라짜식아제발나와라</Modal>}
+      {isModalOpen && (
+        <Modal onClickToggleModal={onClickToggleModal}>{showLogin ? <Login /> : <SignUp />}</Modal>
+      )}
     </div>
   )
 }
