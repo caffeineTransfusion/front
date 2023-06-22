db76d33ab32213ed8d53599f8e1727ce088e8a71
epppo <amorosso@kakao.com>
2023-06-22 23:50:56 +0900

    [ Refactor ] : 파일 구조변경

--- src/component/common/Modal.tsx
+++ src/component/common/Modal.tsx
@@ -1,5 +1,5 @@
 import React, { PropsWithChildren } from 'react'
-import '../../styles/main/Modal.scss'
+import '../../assets/styles/common/Modal.scss'
 import { ModalProps } from '../../types/commonTypes'
 
 const Modal: React.FC<ModalProps> = ({ onClickToggleModal, children }) => {
